70bd9e57a16a5ee2c7a239eed29a6f59a2391054
gabhyun kim <kimgb415@gmail.com>
2025-01-30 17:35:33 +0900

    temporary workaround for huggingface demo

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+import requests
+import zipfile
+import io
 from know_lang_bot.chat_bot.chat_interface import create_chatbot
 from know_lang_bot.config import AppConfig
+import tempfile
+from rich.console import Console
 
+console = Console()
 
-config = AppConfig()
-demo = create_chatbot(config)
-demo.launch()
+# gradio demo
+with tempfile.TemporaryDirectory() as temp_dir:
+    config = AppConfig()
+    with console.status("Downloading and extracting codebase...") as status:
+        config.db.codebase_directory = temp_dir
+        status.update(f"Downloading codebase into {temp_dir}...\n")
+        
+
+        # Download and unzip the code
+        url = "https://github.com/huggingface/transformers/archive/refs/tags/v4.48.1.zip"
+        response = requests.get(url)
+        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+        status.update(f"Extracting codebase...\n")
+
+        # Extract the zip file into the codebase directory
+        zip_file.extractall(config.db.codebase_directory)
+
+    # Create and launch the chatbot
+    demo = create_chatbot(config)
+    demo.launch()
