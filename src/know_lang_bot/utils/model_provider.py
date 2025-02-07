093a34bc2ae47a2e37abb69c0bdee2404ae7ed17
gabhyun kim <kimgb415@gmail.com>
2025-02-08 08:21:45 +0900

    hide huggingface import inside

--- src/know_lang_bot/utils/model_provider.py
+++ src/know_lang_bot/utils/model_provider.py
@@ -1,6 +1,5 @@
 from pydantic_ai.models import Model, KnownModelName
 from know_lang_bot.core.types import ModelProvider
-from know_lang_bot.models.huggingface import HuggingFaceModel
 from typing import get_args
 
 def create_pydantic_model(
@@ -12,6 +11,7 @@ def create_pydantic_model(
     if model_str in get_args(KnownModelName):
         return model_str
     elif model_provider == ModelProvider.HUGGINGFACE:
+        from know_lang_bot.models.huggingface import HuggingFaceModel
         return HuggingFaceModel(model_name=model_name)
     elif model_provider == ModelProvider.TESTING:
         # should be used for testing purposes only
