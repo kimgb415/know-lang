e6b456ae2280b7a747e5ce5fd4322f9ae2d9a0d9
gabhyun kim <kimgb415@gmail.com>
2025-02-10 08:27:23 +0900

    no more temp dir in huggingface demo

--- hf_transformers_demo.py
+++ hf_transformers_demo.py
@@ -1,30 +1,7 @@
-import requests
-import zipfile
-import io
 from knowlang.chat_bot.chat_interface import create_chatbot
 from knowlang.configs.config import AppConfig
-import tempfile
-from rich.console import Console
 
-console = Console()
 
-# gradio demo
-with tempfile.TemporaryDirectory() as temp_dir:
-    config = AppConfig()
-    with console.status("Downloading and extracting codebase...") as status:
-        config.db.codebase_directory = temp_dir
-        status.update(f"Downloading codebase into {temp_dir}...\n")
-        
-
-        # Download and unzip the code
-        url = "https://github.com/huggingface/transformers/archive/refs/tags/v4.48.1.zip"
-        response = requests.get(url)
-        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
-        status.update(f"Extracting codebase...\n")
-
-        # Extract the zip file into the codebase directory
-        zip_file.extractall(config.db.codebase_directory)
-
-    # Create and launch the chatbot
-    demo = create_chatbot(config)
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+config = AppConfig()
+demo = create_chatbot(config)
+demo.launch(server_name="0.0.0.0", server_port=7860)
